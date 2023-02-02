63c66014b376c3af24194d6461c5dfe68c3f28a8
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-02 17:36:40 +0200

    Start over

--- actions/register.php
+++ actions/register.php
@@ -11,31 +11,12 @@ $image = $_FILES['photo']['name'];
 $tmp_name = $_FILES['photo']['tmp_name'];
 $std = $_POST['std'];
 
-$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-$errors = array();
-
-
-if (empty($username) or empty($mobile) or empty($password) or empty($cpassword)) {
-    array_push($errors, "All fields are required");
-}
-if (strlen($password) < 8) {
-    array_push($errors, "Password must be at least 8 charactes long");
-}
-if ($password !== $cpassword) {
-    array_push($errors, "Password does not match");
-}
-require_once "actions/connect.php";
-$sql = "SELECT * FROM userdata WHERE username = '$username'";
-$resultat = mysqli_query($connection, $sql);
-$rowCount = mysqli_num_rows($resultat);
-if ($rowCount > 0) {
-    array_push($errors, "Username already exists!");
-}
-if (count($errors) > 0) {
-    foreach ($errors as  $error) {
-        echo "<div class='alert alert-danger'>$error</div>";
-    }
+if ($password != $cpassword) {
+    echo '<script>
+    alert("Password did not match");
+    window.location="../partials/registration.php"
+    </script>';
 } else {
     move_uploaded_file($tmp_name, "../uploads/$image");
     $sql = "insert into `userdata` (username,mobile,password,photo,standard,status,votes) values ('$username','$mobile','$password','$image','$std',0,0)";
@@ -43,8 +24,8 @@ if (count($errors) > 0) {
 
 
     if ($result) {
-        echo "<div class='alert alert-success'>You are registered successfully.</div>";
         echo '<script>
+        alert("Registration successfull");
         window.location="../";
         </script>';
     } else {
