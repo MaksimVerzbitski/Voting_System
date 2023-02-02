f7202e3d7f62985b654d511816bae774fa82610f
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-02 20:32:02 +0200

    Voting

--- actions/voting.php
+++ actions/voting.php
@@ -10,21 +10,21 @@ $gid = $POST['groupid'];
 $uid = $_SESSION['id'];
 
 
-$updatevotes = mysqli_query($connection, "update my table `userdata` set votes='$totalvotes' where id='$gid'");
-$updatestatus = mysqli_query($connection, "update my table `userdata` set status=1 where id='$uid'");
+$updatevotes = mysqli_query($connection, "update  `userdata` set votes='$totalvotes' where id='$gid'");
+$updatestatus = mysqli_query($connection, "update `userdata` set status=1 where id='$uid'");
 
 if ($updatevotes and $updatestatus) {
-    $getgroups=mysqli_query($connection, "Select username, photo, votes, id from `userdata` where standard='group'");
-    $groups=mysqli_fetch_all($getgroups,MYSQLI_ASSOC);
-    $_SESSION['groups']=$groups;
-    $_SESSION['status']=1;
+    $getgroups = mysqli_query($connection, "Select username, photo, votes, id from `userdata` where standard='group'");
+    $groups = mysqli_fetch_all($getgroups, MYSQLI_ASSOC);
+    $_SESSION['groups'] = $groups;
+    $_SESSION['status'] = 1;
     echo '<script>
     alert("Voting successful");
     window.location="../partials/dashboard.php";
     
     </script>';
-
 } else {
+    echo "Connection failed: " . $connection->connect_error;
     echo '<script>
     alert("Technical error !! Vote after sometime");
     window.location="../partials/dashboard.php";
