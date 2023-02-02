56c706769808d5ea1cc94dd87421757e49eec607
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-02 18:27:21 +0200

    Votes

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -20,7 +20,8 @@ if ($_SESSION['status'] == 1) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="">
     <link rel="stylesheet" href="../styles/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Voting SystemDashboard</title>
 </head>
 
@@ -36,45 +37,45 @@ if ($_SESSION['status'] == 1) {
                     $groups = $_SESSION['groups'];
                     for ($i = 0; $i < count($groups); $i++) {
                 ?>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <img src="../uploads/<?php echo $groups[$i]['photo'] ?>" alt="Group Image">
-                            </div>
-                            <div class="col-md-8">
-                                <strong class="text-dark h5">Group name:</strong><?php echo $groups[$i]['username'] ?>
-                                <br>
-                                <strong class="text-dark h5">Votes:</strong><?php echo $groups[$i]['votes'] ?>
-                                <br>
-                            </div>
-                        </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="../uploads/<?php echo $groups[$i]['photo'] ?>" alt="Group Image">
+                    </div>
+                    <div class="col-md-8">
+                        <strong class="text-dark h5">Group name:</strong><?php echo $groups[$i]['username'] ?>
+                        <br>
+                        <strong class="text-dark h5">Votes:</strong><?php echo $groups[$i]['votes'] ?>
+                        <br>
+                    </div>
+                </div>
 
-                        <form action="../actions/voting.php" method="POST">
-                            <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes'] ?>">
-                            <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id'] ?>">
-                            <?php
+                <form action="../actions/voting.php" method="post">
+                    <input type="hidden" name="groupvotes" value="<?php echo $groups[$i]['votes'] ?>">
+                    <input type="hidden" name="groupid" value="<?php echo $groups[$i]['id'] ?>">
+                    <?php
                             if ($_SESSION['status'] == 1) {
                             ?>
-                                <button class="bg-success my-3 text-white px-3">voted</button>
-                            <?php
+                    <button class="bg-success my-3 text-white px-3">Voted</button>
+                    <?php
                             } else {
                             ?>
-                                <button class="bg-danger my-3 text-white px-3" type="submit">Vote</button>
-                            <?php
+                    <button class="bg-danger my-3 text-white px-3" type="submit">Vote</button>
+                    <?php
                             }
                             ?>
 
-                            <?php
+                    <?php
 
                             ?>
-                        </form>
-                        <hr>
-                    <?php
+                </form>
+                <hr>
+                <?php
                     }
                 } else {
                     ?>
-                    <div class="container">
-                        <p>No groups to display</p>
-                    </div>
+                <div class="container">
+                    <p>No groups to display</p>
+                </div>
                 <?php
 
                 }
