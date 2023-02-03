80f60c5cc987ac8f5496f9be45fea5acabfb2263
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-03 09:28:42 +0200

    Disable Button with js function with library

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -20,6 +20,7 @@ if ($_SESSION['status'] == 1) {
     <link rel="stylesheet" href="../styles/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <title>Voting SystemDashboard</title>
 </head>
 
