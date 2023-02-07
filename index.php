e321b9b9a96599befa4a6ca22a458c19d797fe06
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-02-07 10:51:22 +0200

    Sorting out warnings on same page

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="styles/style.css">
     <title>PHP voting system</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body class="bg-dark">
@@ -18,22 +17,55 @@
     <div class="bg-info py-2">
         <h2 class="text-center py-3">Login</h2>
         <div class="container text-center">
+            <?php
+            session_start();
+            include('connect.php');
+
+            $username = $_POST['username'];
+            $mobile = $_POST['mobile'];
+            $password = $_POST['password'];
+            $std = $_POST['std'];
+
+            $error = "";
+
+            if (empty($username) || empty($mobile) || empty($password) || empty($std)) {
+                $error = "All fields are required. Please fill in all the fields.";
+            } elseif (strlen($password) < 8) {
+                $error = "Password must be at least 8 characters long.";
+            } else {
+                $sql = "SELECT * FROM `userdata` WHERE username='$username' AND mobile='$mobile' AND password='$password' AND standard='$std'";
+                $result = mysqli_query($connection, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    $data = mysqli_fetch_array($result);
+                    $_SESSION['id'] = $data['id'];
+                    $_SESSION['status'] = $data['status'];
+                    $_SESSION['data'] = $data;
+
+                    header("Location: ../partials/dashboard.php");
+                    exit;
+                } else {
+                    $error = "Invalid username or password. Please try again.";
+                }
+            }
+
+            if (!empty($error)) {
+                echo "<p style='color: red;'>" . $error . "</p>";
+            }
+            ?>
             <form action="./actions/login.php" method="POST">
 
                 <div class="mb-3">
-                    <input type="text" class="form-control w-75 m-auto" name="username"
-                        placeholder="Enter your username: " required="required">
+                    <input type="text" class="form-control w-75 m-auto" name="username" placeholder="Enter your username: " required="required">
 
                 </div>
                 <div class="mb-3">
 
-                    <input type="text" class="form-control w-75 m-auto" name="mobile"
-                        placeholder="Enter your mobile number: " required="required" maxlength="10" minlength="10">
+                    <input type="text" class="form-control w-75 m-auto" name="mobile" placeholder="Enter your mobile number: " required="required" maxlength="10" minlength="10">
                 </div>
                 <div class="mb-3">
 
-                    <input type="password" class="form-control w-75 m-auto" name="password"
-                        placeholder="Enter your password: " required="required">
+                    <input type="password" class="form-control w-75 m-auto" name="password" placeholder="Enter your password: " required="required">
 
                 </div>
                 <div class="mb-3">
